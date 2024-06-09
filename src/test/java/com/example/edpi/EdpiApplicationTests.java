461477e3cae0918c534aadbf80dc05920889b35d
yata78 <tatsuya20000923@icloud.com>
2024-06-09 14:15:26 +0900

    first commit

---
+++ src/test/java/com/example/edpi/EdpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
