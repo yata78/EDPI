4300bc61787c644b1b8d7966d0d766f8112f352d
yata78 <tatsuya20000923@icloud.com>
2024-06-09 17:29:00 +0900

    SQL作成

--- SQL/createTable.sql
+++ SQL/createTable.sql
@@ -1,16 +1,17 @@
+DROP TABLE Edpis;
 DROP TABLE Users;
-DROP TABLE Edpi;
 
-create Table Users {
+create Table Users (
     UserId SERIAL PRIMARY KEY,
-    UserName VARCHAR,
-    UserPassword VARCHAR,
+    UserName VARCHAR NOT NULL,
+    UserPassword VARCHAR NOT NULL,
     Created_At TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-};
+);
 
-create Table Edpis {
-    UserId INT FOREIGN KEY,
-    Dpi INT,
-    Sensitivity DECIMAL(1,4),
+create Table Edpis (
+    UserId INTEGER,
+    Dpi INTEGER NOT NULL,
+    Sensitivity DECIMAL(1,4) NOT NULL,
     Created_At TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-};
+	FOREIGN KEY(UserId) REFERENCES Users(UserId)
+);
