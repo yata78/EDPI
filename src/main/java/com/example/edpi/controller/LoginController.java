63b38436cc5b621ef6dba6608110241468e3a002
yata78 <tatsuya20000923@icloud.com>
2024-06-11 22:51:49 +0900

    画面からデータ受け渡し

--- src/main/java/com/example/edpi/controller/LoginController.java
+++ src/main/java/com/example/edpi/controller/LoginController.java
@@ -3,13 +3,18 @@ package com.example.edpi.controller;
 import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.edpi.model.Edpi;
 import com.example.edpi.repository.EdpiRepository;
 import com.example.edpi.repository.UserRepository;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+
 
 
 @Controller
@@ -25,8 +30,9 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ModelAndView postMethodName(ModelAndView mav) {
+    public ModelAndView postMethodName(ModelAndView mav ,@RequestParam("userName") String message, Model model) {
         java.util.List<Edpi> edpis = repository.findAll();
+        mav.addObject("message", message);
         mav.addObject("data", edpis);
         mav.setViewName("main");
         return mav;
