8920501b468430e3307becea288a5c01895529c7
yata78 <tatsuya20000923@icloud.com>
2024-07-29 22:06:17 +0900

    first commit

---
+++ src/main/java/com/example/edpi/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.example.edpi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/", method=RequestMethod.GET)
+    public ModelAndView requestMethodName(ModelAndView mav) {
+        return mav;
+    }
+    
+}
